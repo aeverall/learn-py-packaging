b39c78493723258e8bbf0bf698adecf8ebdfa54e
aeverall <andrew.everall1995@gmail.com>
2022-03-15 13:53:00 +0000

    Type hinst for python code - mypy

--- learn_py_packaging/toy_module.py
+++ learn_py_packaging/toy_module.py
@@ -1,6 +1,12 @@
-def power(x, y):
+from typing import Union
+
+def power(x: float, y: int) -> float:
     return x**y
 
 
-def multiply(x, y):
+def multiply(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
     return x * y
+
+
+def cast_int(x: Union[int, float]) -> int:
+    return x
